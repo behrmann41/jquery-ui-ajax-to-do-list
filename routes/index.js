c47361ad84f5bdd835b00ca0fdb177146ac22c94
Ian Behrmann <ianjbehrmann@gmail.com>
2015-10-16 10:47:55 -0600

    draggable / dropable working

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var router = express.Router();
+var db = require('monk')('localhost/to-do-list')
+var Tasks = db.get('tasks')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
