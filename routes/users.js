6fb36d51d7923425fb144f48e6fe44ca5976ac7b
Ian Behrmann <ianjbehrmann@gmail.com>
2015-10-17 14:43:11 -0600

    added bcrypt, cookie session and register page

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var router = express.Router();
+var db = require('monk')('localhost/to-do-list')
+var Users = db.get('users')
+
 
 /* GET users listing. */
 router.get('/register', function(req, res, next) {
-  res.send('respond with a resource');
+  res.render('users/register', {  title: "Create an Account"});
 });
 
 module.exports = router;
